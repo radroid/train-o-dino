39cb7f76835018cb23d366091711fbbd7dd8b12b
PineTree007 <91689040+PineTree007@users.noreply.github.com>
2022-03-28 22:48:25 -0400

    MMP - Latest changes
    Changes to Game and model

--- game/js/game.js
+++ game/js/game.js
@@ -170,13 +170,13 @@
 
 			this.removeOldCacti();
 
-			// if(flag == 'true'){
-			// 	this.updateCacti();
-			// 	localStorage.setItem('addCacti',false);				
-			// 	console.log('Step', localStorage.getItem('addCacti'));
-			// }
-			//localStorage.setItem('addCacti',false)
-			//document.getElementById('addCactus').addEventListener('click',this.addCactiBtn);
+			if(flag == 'true'){
+				this.updateCacti();
+				localStorage.setItem('addCacti',false);				
+				console.log('Step', localStorage.getItem('addCacti'));
+			}
+			localStorage.setItem('addCacti',false)
+			document.getElementById('addCactus').addEventListener('click',this.addCactiBtn);
 			this.updateCacti();
 
 			if (!this.player.isJumping(this.offset) && spacePressed) {
